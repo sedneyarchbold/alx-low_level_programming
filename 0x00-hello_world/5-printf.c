6124eadb565fe9d1d4e64a1bc8712e949d8d7fb7
sedneyarchbold <sedneyarchbold9@gmail.com>
2023-06-16 01:59:16 -0700

    t1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
-#include<stdio.h>
-  2 /**
-  3   *main - Entry point 
-  4   *
-  5   *Return: Always 0 (Success)
-  6   */
-  7 int main(void)
-  8 {
-  9         printf("with proper grammar, but the outcome is a piece of art,\n");
- 10         return (0);
- 11 }
+#include <stdio.h>
+/**
+ * main - A program that print a line using the print  * Retrun: 0 (success)
+ */
+int main(void)
+ {
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
